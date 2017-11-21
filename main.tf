6d0c50cc9e16942b85f59266f36effb6dd750f65
Manuel Oloarte <manuel@oloarte.ch>
2017-11-21 17:32:21 +0200

    adding aws security group and defining user_data inside aws_instance.example

--- main.tf
+++ main.tf
@@ -3,10 +3,28 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    ami = "ami-40d28157"
-    instance_type = "t2.micro"
-    
+    ami                    = "ami-40d28157"
+    instance_type          = "t2.micro"
+    vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+
+    user_data = "${data.template_file.shell_script_example.rendered}" 
+
     tags = {
         Name = "terraform-example"
     }
+}
+
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+
+    ingress {
+        from_port   = 8080
+        to_port     = 8080
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+data "template_file" "shell_script_example" {
+  template = "${file(format("%s/templates/sh.template", path.module))}"
 }
