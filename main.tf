b175017497c4440524efb0d000b4a2b937fe2128
Manuel Oloarte <manuel@oloarte.ch>
2017-11-22 11:35:32 +0200

    removing hardcoded server port. allowing ssh access

--- main.tf
+++ main.tf
@@ -1,6 +1,14 @@
 provider "aws" {
     region = "us-east-1"
 }
+variable "server_port" { 
+    description = "The port the server will use for HTTP requests"
+    default = 8080
+}
+variable "ssh_port" { 
+    description = "The port the server will use for SSH requests"
+    default = 22
+}
 
 resource "aws_instance" "example" {
     ami                    = "ami-40d28157"
@@ -18,13 +26,32 @@ resource "aws_security_group" "instance" {
     name = "terraform-example-instance"
 
     ingress {
-        from_port   = 8080
-        to_port     = 8080
+        from_port   = "${var.server_port}"
+        to_port     = "${var.server_port}"
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+resource "aws_security_group" "ssh_in" {
+    name = "terraform-ssh-instance"
+
+    ingress {
+        from_port   = "${var.ssh_port}"
+        to_port     = "${var.ssh_port}"
         protocol    = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
 }
 
 data "template_file" "shell_script_example" {
-  template = "${file(format("%s/templates/sh.template", path.module))}"
+    template = "${file(format("%s/templates/sh.template", path.module))}"
+    vars {
+        server_port = "${var.server_port}"
+    }
+}
+output "public_ip" {
+    value = "${aws_instance.example.public_ip}"
+}
+output "server_port" {
+   value = "${var.server_port}"
 }
