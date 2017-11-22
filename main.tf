69c7ced08f24c9eefe79743623d13195a2fe49ac
Manuel Oloarte <manuel@oloarte.ch>
2017-11-22 12:22:21 +0200

    converting from single server to autoscaling group

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 provider "aws" {
     region = "us-east-1"
 }
+# Variable Section
 variable "server_port" { 
     description = "The port the server will use for HTTP requests"
     default = 8080
@@ -9,16 +10,30 @@ variable "ssh_port" {
     description = "The port the server will use for SSH requests"
     default = 22
 }
-
-resource "aws_instance" "example" {
-    ami                    = "ami-40d28157"
-    instance_type          = "t2.micro"
-    vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+# Declaring resources
+resource "aws_launch_configuration" "example" {
+    image_id        = "ami-40d28157"
+    instance_type   = "t2.micro"
+    security_groups = ["${aws_security_group.instance.id}"]
 
     user_data = "${data.template_file.shell_script_example.rendered}" 
 
-    tags = {
-        Name = "terraform-example"
+    lifecycle {
+        create_before_destroy = true
+    }
+}
+
+resource "aws_autoscaling_group" "example" {
+    launch_configuration = "${aws_launch_configuration.example.id}"
+    availability_zones   = ["${data.aws_availability_zones.all.names}"]
+
+    min_size = 2
+    max_size = 10
+
+    tag {
+        key                 = "Name"
+        value               = "terraform-asg-example"
+        propagate_at_launch = true
     }
 }
 
@@ -31,6 +46,9 @@ resource "aws_security_group" "instance" {
         protocol    = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+    lifecycle {
+        create_before_destroy = true
+    }
 }
 resource "aws_security_group" "ssh_in" {
     name = "terraform-ssh-instance"
@@ -41,17 +59,20 @@ resource "aws_security_group" "ssh_in" {
         protocol    = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+    lifecycle {
+        create_before_destroy = true
+    }
 }
-
+# Script to provision
 data "template_file" "shell_script_example" {
     template = "${file(format("%s/templates/sh.template", path.module))}"
     vars {
         server_port = "${var.server_port}"
     }
 }
-output "public_ip" {
-    value = "${aws_instance.example.public_ip}"
-}
+# State can be either: available, information, impaired, or unavailable
+data "aws_availability_zones" "all" {}
+# Output things
 output "server_port" {
    value = "${var.server_port}"
 }
